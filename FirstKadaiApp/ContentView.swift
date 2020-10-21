3b39402d397c68d5644d7e87de7868e1ac2bb1e2
shin.ota <shin.ota0817@gmail.com>
2020-10-21 16:25:31 +0900

    Initial Commit

---
+++ FirstKadaiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstKadaiApp
+//
+//  Created by Shin Ota on 10/21/20.
+//  Copyright © 2020 Shin Ota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
